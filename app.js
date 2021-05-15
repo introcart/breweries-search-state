e44b930d528b400d4102cfdb47530ae51e40ce8f
Steven Carton <spc2582@icloud.com>
2021-05-15 17:03:12 -0400

    first version of simple state brewery app complete

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-async function getBreweries(){
-  const res = await axios.get('https://api.openbrewerydb.org/breweries?by_state=ohio&per_page=50')
-  const div = document.querySelector('#breweries')
-  for(let brewery of res.data){
-    const h5 = document.createElement('h5')
-    h5.innerText = brewery.name
-    const h6 = document.createElement('h6')
-    h6.innerHTML = `<a href=${brewery.website_url}>${brewery.name}</a>`
-    div.append(h5)
-    div.append(h6)
-    
-   
+
+const API = 'https://api.openbrewerydb.org/breweries?per_page=50'
+
+async function getBreweries(state){
+  for (let i = 0; i <= 20; i++){
+    const res = await axios.get(`${API}&by_state=${state}&page=${i}}`)
+    for(let brewery of res.data){
+      const div = document.createElement('div')
+      div.classList.add('card')
+      div.innerHTML = `
+        <div class="card-body">
+          <h5 class="card-title" id="name">${brewery.name}</h5>
+          <h6 class="card-subtitle mb-2 text-muted" id="location">${brewery.city}</h6>
+          <a href="${brewery.website_url}" class="card-link" id="link">${brewery.website_url}</a>
+        </div>
+      `
+      document.querySelector('.container').append(div)
+    }
   }
+}
+ 
+const form = document.querySelector('#breweryForm')
+const stateInput = document.querySelector('input[name="state"]');
+form.addEventListener('submit', function(evt){
+  evt.preventDefault();
+  getBreweries(stateInput.value)
+})
+
+
+
+
   // const res2 = await axios.get('https://api.openbrewerydb.org/breweries?by_state=ohio&per_page=50&page=2')
   // for(let brewery of res2.data){
   //   const li = document.createElement('li')
@@ -55,7 +73,6 @@ async function getBreweries(){
   // }
   
 
-}
 
 
 
